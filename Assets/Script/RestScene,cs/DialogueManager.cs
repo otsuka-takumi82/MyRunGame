7df7b5ba61544a7dd82ea1fc240f2183c2a50f81
gongdazhong79 <gongdazhong79@gmail.com>
2026-06-24 02:33:30 +0900

    [Add]ピエロ会話追加（スコアによって見える）

--- Assets/Script/RestScene,cs/DialogueManager.cs
+++ Assets/Script/RestScene,cs/DialogueManager.cs
@@ -6,14 +6,17 @@ public class DialogueManager : MonoBehaviour
 {
     [SerializeField, Header("NPC")]
     private GameObject[] _npc;
-    [SerializeField, Header("セリフ")]
-    private Text _npc1Coment;
+    [SerializeField, Header("テキストボックス")]
+    private Text _textBox;
     [SerializeField, Header("ボタン")]
     private GameObject _button;
+    [SerializeField, Header("選択肢")]
+    private GameObject _ChoisesButton;
 
     private bool _isTolking;
+    private int _YesNo = 0;
     private GameManager _gameManager;
-    public int num = 0;
+    private int num = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -22,26 +25,82 @@ public class DialogueManager : MonoBehaviour
 
     private void Start()
     {
-        if (_gameManager._boxStage > 5) return;
+        if (_gameManager._boxStage > 5 || _gameManager._boxStage == 3) return;
         _npc[_gameManager._boxStage - 1].SetActive(true);
+        _button.gameObject.SetActive(true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_isTolking);
         Dialog();
     }
 
-    private string[] _message =
+    private string[] _npc1Message =
     {
-        "おはよう",
-        "ころそうか",
-        "好きだ"
+        "こんばんは",
+        "正直に、人は殺したかの？",
+        "正直でよろしい。いいこと教えよう。",
+        "この先は盗賊が多い。お前さんなら敵じゃあるまい。",
+        "やっておしまいよ"
+    };
+
+    private string[] _npc1Message2 =
+    {
+        "",
+        "",
+        "う、嘘をつけい！",
+        "わしは見た！見たんじゃからのう！",
+        "マイケルはいいやつじゃった。うう,,,"
+    };
+
+    private string[] _npc2Message =
+    {
+        "こんばんは",
+        "盗賊を倒してくれてありがとう",
+        "秘密知りたいですか？",
+        "宮廷の道化師さんって結構残虐な",
+        "性格しているんです",
+        "気を付けて"
+    };
+
+    private string[] _npc2Message2 =
+    {
+        "",
+        "",
+        "",
+        "では、また"
+    };
+
+    private string[] _npc3Message =
+    {
+        "こんばんは",
+        "君気に入ったよ、ひひっ",
+        "君は王が正しいと思うかい？？",
+        "おっと失礼",
+        "お嬢様を忘れてましたよ",
+        "ヒヒッ"
+    };
+
+    private string[] _npc3Message2 =
+    {
+        "",
+        "",
+        "",
+        "HAHAHAHAHA",
+        "何を大人ぶってるんだい坊や",
+        "王様好きになるようなこと教えてアゲル！！",
+        "王様は見栄っ張りなんだよね。HAHA",
+        "あんなの食えるわけないのにネ！",
+        "内心なくならないかと思っているだろうさ！",
+        "みじめだね。ひひっ"
     };
 
     public void NPC1()
     {
-        Destroy(_button.gameObject);
+        _button.gameObject.SetActive(false);
 
         _isTolking = true;
         
@@ -50,23 +109,168 @@ public class DialogueManager : MonoBehaviour
         //_npc1Coment.text = _message[num]; 
     }
 
+    public void Yes()
+    {
+        _YesNo = 1;
+        num++;
+    }
+    public void No()
+    {
+        _YesNo = 2;
+        num++;
+    }
+
     public void Dialog()
     {
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !_ChoisesButton.gameObject.activeSelf && _textBox.gameObject.activeSelf)
         {
             num++;
         }
 
-        if (num < _message.Length && _isTolking == true)
+        //ステージ１後
+        if (_gameManager._boxStage == 1)
         {
-            _npc1Coment.gameObject.SetActive(true);
-            _npc1Coment.text = _message[num];
-            
+            if (_isTolking == true)
+            {
+                _textBox.gameObject.SetActive(true);
+                if (num == 1)
+                {
+                    _ChoisesButton.gameObject.SetActive(true);
+
+                }
+                else
+                {
+                    _ChoisesButton.gameObject.SetActive(false);
+                }
+                if (_YesNo <= 1)
+                {
+                    if(num < _npc1Message.Length)
+                    {
+                        _textBox.text = _npc1Message[num];
+                    }
+                    else
+                    {
+                        _textBox.gameObject.SetActive(false);
+                    }
+                }
+                else if (_YesNo == 2)
+                {
+                    if (num < _npc1Message2.Length)
+                    {
+                        _textBox.text = _npc1Message2[num];
+                    }
+                    else
+                    {
+                        _textBox.gameObject.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                _textBox.gameObject.SetActive(false);
+                _isTolking = false;
+
+            }
         }
-        else
+        //ステージ2後
+        else if (_gameManager._boxStage == 2)
         {
-            _npc1Coment.gameObject.SetActive(false);
+            if (_isTolking == true)
+            {
+                _textBox.gameObject.SetActive(true);
+                if (num == 2)
+                {
+                    _ChoisesButton.gameObject.SetActive(true);
+
+                }
+                else
+                {
+                    _ChoisesButton.gameObject.SetActive(false);
+                }
+                if (_YesNo <= 1)
+                {
+                    if (num < _npc2Message.Length)
+                    {
+                        _textBox.text = _npc2Message[num];
+                    }
+                    else
+                    {
+                        _textBox.gameObject.SetActive(false);
+                    }
+                }
+                else if (_YesNo == 2)
+                {
+                    if (num < _npc2Message2.Length)
+                    {
+                        _textBox.text = _npc2Message2[num];
+
+                    }
+                    else
+                    {
+                        _textBox.gameObject.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                _textBox.gameObject.SetActive(false);
+                _isTolking = false;
+
+            }
+        }
+
+        //ステージ3後
+        else if (_gameManager._boxStage == 3 && _gameManager._boxScore >= 100)
+        {
+            _npc[_gameManager._boxStage - 1].SetActive(true);
+            _button.gameObject.SetActive(true);
+            if (_isTolking == true)
+            {
+                _button.gameObject.SetActive(false);
+                _textBox.gameObject.SetActive(true);
+                if (num == 2)
+                {
+                    _ChoisesButton.gameObject.SetActive(true);
+
+                }
+                else
+                {
+                    _ChoisesButton.gameObject.SetActive(false);
+                }
+                if (_YesNo <= 1)
+                {
+                    if (num < _npc3Message.Length)
+                    {
+                        _textBox.text = _npc3Message[num];
+                    }
+                    else
+                    {
+                        _textBox.gameObject.SetActive(false);
+                        
+                    }
+                }
+                else if (_YesNo == 2)
+                {
+                    if (num < _npc3Message2.Length)
+                    {
+                        _textBox.text = _npc3Message2[num];
+
+                    }
+                    else
+                    {
+                        _textBox.gameObject.SetActive(false);
+                        
+                    }
+                }
+            }
+            else
+            {
+                _textBox.gameObject.SetActive(false);
+                _button.gameObject.SetActive(true);
+                
+
+            }
         }
     }
 
--- Assets/Script/TitleScene,cs/GameManager.cs
+++ Assets/Script/TitleScene,cs/GameManager.cs
@@ -27,6 +27,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_boxStage);
+        //Debug.Log(_boxStage);
     }
 }
