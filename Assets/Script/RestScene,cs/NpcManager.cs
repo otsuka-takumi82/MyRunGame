b313b04d8e75370326d0b587d08c90847f43bdb3
gongdazhong79 <gongdazhong79@gmail.com>
2026-07-12 08:22:15 +0900

    [Add]武器チュートリアル追加

--- Assets/Script/RestScene,cs/NpcManager.cs
+++ Assets/Script/RestScene,cs/NpcManager.cs
@@ -16,16 +16,22 @@ public class NpcManager : MonoBehaviour
     private string[] _dialog2;
     [SerializeField, Header("会話ボックス")]
     private GameObject _textBox;
+    [SerializeField, Header("説明ボックス")]
+    private GameObject[] _planBox;
 
     public int _indexNum;
     private GameManager _gameManager;
     private bool _crossBow;
     private bool _bigSword;
     private bool _canon;
+    private bool _isPlanIng;
+    private bool _isTolking;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _gameManager = FindFirstObjectByType<GameManager>();
+        _isTolking = true;
+        _isPlanIng = true;
     }
 
     // Update is called once per frame
@@ -52,29 +58,40 @@ public class NpcManager : MonoBehaviour
         
         if(GameManagerNum(11))
         {
-            NPCDialog(_dialog);
+            NPCDialog(_dialog, 0);
         }
         else if (GameManagerNum(12))
         {
-            NPCDialog(_dialog1);
+            NPCDialog(_dialog1, 1);
         }
         else if (GameManagerNum(13))
         {
-            NPCDialog(_dialog2);
+            NPCDialog(_dialog2, 2);
         }
 
 
     }
 
-    public void NPCDialog(string[] text)
+    public void NPCDialog(string[] text, int index)
     {
         if(_indexNum < text.Length)
         {
             _tolk.text = text[_indexNum];
+            if(_isTolking && _indexNum > 0)
+            {
+                _textBox.SetActive(true);
+                _isTolking = false;
+            }
         }
         else
         {
-            _textBox.SetActive(false);
+            if(_isPlanIng)
+            {
+                _textBox.SetActive(false);
+                _planBox[index].SetActive(true);
+                _isPlanIng = false;
+            }
+            
         }
         
     }
@@ -101,6 +118,26 @@ public class NpcManager : MonoBehaviour
     public bool GameManagerNum(int num)
     {
         return _gameManager._boxStage == num;
+    }
+
+    public void OnOk(int index, bool wepon)
+    {
+        _isPlanIng = false;
+        wepon = true;
+        _planBox[index].SetActive(false );
+    }
+
+    public void OnOK0()
+    {
+        OnOk(0, _canon);
+    }
+    public void OnOK1()
+    {
+        OnOk(1, _crossBow);
+    }
+    public void OnOK2()
+    {
+        OnOk(2, _bigSword);
     }
 
 
